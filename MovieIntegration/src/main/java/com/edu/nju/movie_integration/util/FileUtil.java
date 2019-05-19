a08fbee938d49c4912f3236378d203b734c61f2a
2529716798 <2529716798@qq.com>
2019-05-19 13:03:12 +0800

    Fix the bug of douban_film && format the dataset postion // resources

---
+++ MovieIntegration/src/main/java/com/edu/nju/movie_integration/util/FileUtil.java
@@ -0,0 +1,9 @@
+package com.edu.nju.movie_integration.util;
+
+/**
+ * @Author: py
+ * @Date: 2019-05-19 12:43
+ * @Version 1.0
+ */
+public class FileUtil {
+}
---
+++ MovieIntegration/src/main/java/com/edu/nju/movie_integration/util/Json2ObjUtil.java
@@ -0,0 +1,9 @@
+package com.edu.nju.movie_integration.util;
+
+/**
+ * @Author: py
+ * @Date: 2019-05-19 12:33
+ * @Version 1.0
+ */
+public class Json2ObjUtil {
+}
