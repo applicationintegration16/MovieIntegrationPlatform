a82fd038a25cd32ab0410d67f901e9e76782731d
julia98 <925583000@qq.com>
2019-05-19 11:53:17 +0800

    mock

---
+++ MovieIntegration/src/main/java/com/edu/nju/movie_integration/controller/UIController.java
@@ -0,0 +1,31 @@
+package com.edu.nju.movie_integration.controller;
+
+import com.edu.nju.movie_integration.domain.Movie;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class UIController {
+    @RequestMapping("/movie")
+    public List<Movie> getMovieList() {
+        Movie test1 = new Movie();
+        test1.setName("1");
+        List<String> descriptions1 = new ArrayList<>();
+        descriptions1.add("test123");
+        test1.setDescription(descriptions1);
+
+        Movie test2 = new Movie();
+        test1.setName("2");
+        List<String> descriptions2 = new ArrayList<>();
+        descriptions2.add("test321");
+        test1.setDescription(descriptions2);
+
+        List<Movie> list = new ArrayList<>();
+        list.add(test1);
+        list.add(test2);
+        return list;
+    }
+}
